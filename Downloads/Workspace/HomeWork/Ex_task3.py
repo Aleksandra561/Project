86551becfd4f389d7d605ac4d9dd69cd9ebb2107
Aleksandra <aleksandra-kuskova@yandex.ru>
2025-07-29 07:54:10 +0300

    initial commit

--- Downloads/Workspace/HomeWork/Ex_task3.py
+++ Downloads/Workspace/HomeWork/Ex_task3.py
@@ -69,17 +69,20 @@ for country in my_list:
     country_dict_rev[country] = country_id_counter
     country_id_counter += 1 
 #print (country_dict_rev)
-countries = open (dirfile+'countries.csv','w')
+countries = open (dirfile+'countries.csv','w', encoding='utf-8')
 for i in my_list:
     countries.write(f"{counter}\t {i}\n")
+    print (f"{counter}\t {i}\n")
     counter +=1
 countries.close()
 
+
 state_city_list =set()
-row_count = 0
+#row_count = 0
 state_city_dict=dict()
 state_city_dict_rev=dict()
 sity_state_id = 0
+dirfile = r"C:\Users\95004\Downloads\Workspace\HomeWork\\"
 Export = 'Export.csv'.strip()
 for line in open (Export, encoding='utf-8'):
     mainline = line.strip().split(',')
@@ -88,55 +91,55 @@ for line in open (Export, encoding='utf-8'):
         index = mainline[11].strip()
         state = mainline[10].strip()
         city = mainline[8].strip()
-        state_city = f"{index}\t{country_id_counter}\t{state}\t{city}"
         country = mainline[9].strip()
+        country_id = country_dict_rev.get(country, -1)
+        if country_id!=-1:
+            state_city = f"{index}\t{country_id}\t{state}\t{city}"
     if state_city:
         state_city_list.add(state_city)
 my_list = list(state_city_list)
 print (my_list)
 print(type(my_list))
 dirfile = r"C:\Users\95004\Downloads\Workspace\HomeWork\\"
-counter = 1
+#counter = 1
 for state_city in my_list:
     state_city_dict[sity_state_id] = state_city
     state_city_dict_rev[state_city] = sity_state_id
     sity_state_id += 1 
-state_cities = open (dirfile+'state_cities.csv','w')
+state_cities = open (dirfile+'state_cities.csv','w', encoding='utf-8')
 for i in my_list:
     index, country, state, city = i.split('\t')
-    country_id = country_dict_rev.get(country, -1)  
-    state_cities.write(f"{i}\t{country_id}\n")
+    #country_id = country_dict_rev.get(country, -1)
+    encoding='utf-8'
+    state_cities.write(f"{i}\n")
 state_cities.close()
+print(f"{i}\n")
+
 
 market_list =set()
+dirfile = r"C:\Users\95004\Downloads\Workspace\HomeWork\\"
 Export = 'Export.csv'.strip()
 for line in open (Export, encoding='utf-8'):
     mainline = line.strip().split(',')
-    market = None
-    country = mainline[9].strip()
-    street = mainline[6].strip()
-    #product = mainline[28:58]
-    #my_list = pay[23:28]
-    if len(mainline)>20:
+    if len(mainline)>21:
         FMID = mainline[0].strip()
         market_name = mainline[1].strip()
+        street = mainline[7].strip()
         index = mainline[11].strip()
         lan = mainline[20].strip()
         lon = mainline[21].strip()
-        markets = f"{FMID}\t{market_name}\t{street}\t{index}\t{lan}\t{lon}"
-    
-    if markets:
+        country = mainline[9].strip()
+        markets = f"{FMID}\t{market_name}\t{street}\t{index}\t{lan}\t{lon}\t{country}"
         market_list.add(markets)
 my_list = list(market_list)
 print (my_list)
-print(type(my_list))
-dirfile = r"C:\Users\95004\Downloads\Workspace\HomeWork\\"
+#print(type(my_list))
 markets = open (dirfile+'markets.csv','w')
 for i in my_list:
-   FMID,market_name,street, index,lan,lon = i.split('\t') 
+   FMID,market_name,street, index,lan,lon,country = i.split('\t') 
    country_id = country_dict_rev.get(country, -1)
    #product_id = product_dict_rev.get(product, -1)
    #payment_method_id =  payment_method_dict_rev.get(i, -1)
    state_city_id = state_city_dict_rev.get(state_city, -1)
-   markets.write(f"{FMID}\t{market_name}\t{street}\t{index}\t{lan}\t{lon}\t{country_id}\t{state_city_id}\n")  
+   markets.write(f"{FMID}\t{market_name}\t{street}\t{index}\t{lan}\t{lon}\t{country_id}\n")  
 markets.close()
